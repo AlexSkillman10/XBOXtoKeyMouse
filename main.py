64b7f530ff53a91a7758c350e3e9640709cafe7a
Alex Skillman <alexskillman10@gmail.com>
2023-02-14 00:14:17 -0600

    added feature to pause and resume inputs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-import subprocess
+
 import buttonBindings as bindings
 from XboxController import XboxController
 
@@ -7,7 +7,6 @@ from pynput.keyboard import Key, Controller as KeyboardController
 from pynput.mouse import Button, Controller as MouseController
 
 import pydirectinput
-
 
 import time
 
@@ -19,9 +18,11 @@ def is_admin():
   except:
       return False
 
+
 class ButtonFunctions:
   def __init__(self):
     self.left_mouse_state = 0
+    self.program_active_state = True
   
   def key_press(self, button):
     keyboard.press(button)
@@ -34,6 +35,16 @@ class ButtonFunctions:
           pydirectinput.mouseDown()
         else:
           pydirectinput.mouseUp()
+
+  def flip_active_state(self):
+    if self.program_active_state:
+      self.program_active_state = False
+    else:
+      self.program_active_state = True
+    print('Program State: ' + ('Paused' if not self.program_active_state else 'Active'))
+    
+  def get_active_state(self):
+    return self.program_active_state
 
   def mouse_press(self, button):
       mouse.press(button)
@@ -60,43 +71,50 @@ if __name__ == '__main__' and is_admin():
   button_update_counter = 0
 
   enable_function = True
+
   while 1:
     
     controller_values = Xbox.get_value()
-    ##mouse movement
-    move_x = controller_values['ABS_X']
-    move_y = controller_values['ABS_Y']
-    deadzone = bindings.joystick_deadzone
 
-    if abs(move_x) < deadzone:
-      move_x = 0
-    if abs(move_y) < deadzone:
-      move_y = 0
+    if controller_values['BTN_SELECT'] and controller_values['BTN_START']:
+      button_functions.flip_active_state()
+      time.sleep(0.3)
 
-    if controller_values['BTN_THUMBL'] == 1:
-      mouse.scroll(0, move_y * .4)
-    else:
-      base_sens = bindings.joystick_sensitivity_base_multiplier
-      sens_curve = bindings.joystick_applied_power_curve
-      mouse.move(pow(move_x * base_sens, sens_curve), pow(move_y * -base_sens, sens_curve))
+    if button_functions.get_active_state():
+      ##mouse movement
+      move_x = controller_values['ABS_X']
+      move_y = controller_values['ABS_Y']
+      deadzone = bindings.joystick_deadzone
 
-    ##binary_buttons
-    if button_update_counter >= button_update_cycle:
-      for key in bindings.button_binary_keys.keys():
+      if abs(move_x) < deadzone:
+        move_x = 0
+      if abs(move_y) < deadzone:
+        move_y = 0
 
-        action = bindings.button_binary_keys[key]
+      if controller_values['BTN_THUMBL'] == 1:
+        mouse.scroll(0, move_y * .4)
+      else:
+        base_sens = bindings.joystick_sensitivity_base_multiplier
+        sens_curve = bindings.joystick_applied_power_curve
+        mouse.move(pow(move_x * base_sens, sens_curve), pow(move_y * -base_sens, sens_curve))
 
-        if key in ['ABS_HAT0Y', 'ABS_HAT0X']:
-          action = action[controller_values[key]]
+      ##binary_buttons
+      if button_update_counter >= button_update_cycle:
+        for key in bindings.button_binary_keys.keys():
 
-        if action == Button.left:
-          button_functions.change_mouse_left_state(controller_values[key])
-        elif action != None and abs(controller_values[key]) > .02:
-            button_functions.single_press(action)
-            button_update_counter=0
-    
-    button_update_counter += 1
-    time.sleep(0.01)
+          action = bindings.button_binary_keys[key]
+
+          if key in ['ABS_HAT0Y', 'ABS_HAT0X']:
+            action = action[controller_values[key]]
+
+          if action == Button.left:
+            button_functions.change_mouse_left_state(controller_values[key])
+          elif action != None and abs(controller_values[key]) > .02:
+              button_functions.single_press(action)
+              button_update_counter=0
+      
+      button_update_counter += 1
+      time.sleep(0.01)
 else:
   ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
 
